bd16fd546e30e2def9a91efa6af654bfb46cafc1
Michael Parekh <mparekh4@illinois.edu>
2023-05-25 18:19:51 -0400

    init proj

---
+++ Swipeable Cards Demo/Swipeable Cards Demo/Swipeable_Cards_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swipeable_Cards_DemoApp.swift
+//  Swipeable Cards Demo
+//
+//  Created by Michael Parekh on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Swipeable_Cards_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
